e3f84e8bca58b5ddab00d995bf141e2db8a1cdc6
Yoann Gern <yoann@yoanngern.ch>
2015-10-05 18:43:09 +0200

    change host for french

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new iahm\PageBundle\iahmPageBundle(),
             new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
